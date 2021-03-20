82dc42852a3272aad6c5e37f5e43aa193e8873e7
Alex Rueb <rueba@oregonstate.edu>
2021-03-20 15:48:28 -0500

    organized jest tests into hierarchy

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,6 +1,6 @@
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
-import { addBug, resolveBug, getUnresolvedBugs } from "../bugs";
+import { loadBugs, addBug, resolveBug, getUnresolvedBugs } from "../bugs";
 import configureStore from "../configureStore";
 
 describe("bugsSlice", () => {
@@ -20,55 +20,69 @@ describe("bugsSlice", () => {
     },
   });
 
-  it("should add the bug to the store if saved to the server", async () => {
-    // Arrange
-    const bug = { description: "a" };
-    const savedBug = { ...bug, id: 1 };
-    fakeAxios.onPost("/bugs").reply(200, savedBug);
+  describe("resolving bugs", () => {
+    it("should add the bug to the store if saved to the server", async () => {
+      // Arrange
+      const bug = { description: "a" };
+      const savedBug = { ...bug, id: 1 };
+      fakeAxios.onPost("/bugs").reply(200, savedBug);
 
-    // Act
-    await store.dispatch(addBug(bug));
+      // Act
+      await store.dispatch(addBug(bug));
 
-    // Assert
-    expect(bugsSlice().list).toContainEqual(savedBug);
+      // Assert
+      expect(bugsSlice().list).toContainEqual(savedBug);
+    });
+
+    it("should not add the bug to the store if it's not saved to the server", async () => {
+      // Arrange
+      const bug = { description: "a" };
+      fakeAxios.onPost("/bugs").reply(500);
+
+      // Act
+      await store.dispatch(addBug(bug));
+
+      // Assert
+      expect(bugsSlice().list).toHaveLength(0);
+    });
   });
 
-  it("should not add the bug to the store if it's not saved to the server", async () => {
-    // Arrange
-    const bug = { description: "a" };
-    fakeAxios.onPost("/bugs").reply(500);
+  describe("resolving bugs", () => {
+    it("should mark the bug as resolved if saved to the server", async () => {
+      //arrange
+      fakeAxios.onPost("/bugs").reply(200, { id: 1, resolved: false });
+      fakeAxios.onPatch("/bugs/1").reply(200, { id: 1, resolved: true });
 
-    // Act
-    await store.dispatch(addBug(bug));
+      //act
+      await store.dispatch(addBug({}));
+      await store.dispatch(resolveBug(1));
 
-    // Assert
-    expect(bugsSlice().list).toHaveLength(0);
+      //assert
+      expect(bugsSlice().list[0].resolved).toBe(true);
+    });
+
+    it("should not mark the bug as resolved if not saved to the server", async () => {
+      //arrange
+      fakeAxios.onPost("/bugs").reply(200, { id: 1, resolved: false });
+      fakeAxios.onPatch("/bugs/1").reply(500);
+
+      //act
+      await store.dispatch(addBug({}));
+      await store.dispatch(resolveBug(1));
+
+      //assert
+      expect(bugsSlice().list[0].resolved).not.toBe(true);
+    });
   });
 
-  it("should mark the bug as resolved if saved to the server", async () => {
-    //arrange
-    fakeAxios.onPost("/bugs").reply(200, { id: 1, resolved: false });
-    fakeAxios.onPatch("/bugs/1").reply(200, { id: 1, resolved: true });
+  describe("loading bugs", () => {
+    it("Should add bugs to state if api call successful", async () => {
+      fakeAxios.onGet("/bugs").reply(200, [{ id: 1 }, { id: 2 }, { id: 3 }]);
 
-    //act
-    await store.dispatch(addBug({}));
-    await store.dispatch(resolveBug(1));
-
-    //assert
-    expect(bugsSlice().list[0].resolved).toBe(true);
-  });
-
-  it("should not mark the bug as resolved if not saved to the server", async () => {
-    //arrange
-    fakeAxios.onPost("/bugs").reply(200, { id: 1, resolved: false });
-    fakeAxios.onPatch("/bugs/1").reply(500);
-
-    //act
-    await store.dispatch(addBug({}));
-    await store.dispatch(resolveBug(1));
-
-    //assert
-    expect(bugsSlice().list[0].resolved).not.toBe(true);
+      const result = await store.dispatch(loadBugs());
+      console.log(result);
+      expect(bugsSlice().list).toHaveLength(3);
+    });
   });
 
   describe("selectors", () => {
